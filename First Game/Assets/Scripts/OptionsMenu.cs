803488765a65d59e3d7b42460d5243cb114c7ff4
Adam Kover <adamkover47@gmail.com>
2021-03-12 13:37:16 -0800

    options menu update
    sensitivity value shown in text

--- First Game/Assets/Scripts/OptionsMenu.cs
+++ First Game/Assets/Scripts/OptionsMenu.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     private Slider enemySlider;
     private Slider sensitivitySlider;
+    private TextMeshProUGUI sensitivityText;
 
     // Start is called before the first frame update
     void Start()
     {
         enemySlider = GameObject.Find("EnemyHealthSlider").GetComponent<Slider>();
         sensitivitySlider = GameObject.Find("SensitivitySlider").GetComponent<Slider>();
+        sensitivityText = GameObject.Find("SensitivityValue").GetComponent<TextMeshProUGUI>();
         enemySlider.value = PlayerPrefs.GetInt("enemyHealth");
         sensitivitySlider.value = PlayerPrefs.GetFloat("playerSens");
+        sensitivityText.text = sensitivitySlider.value.ToString("#.##");
+    }
+
+
+    public void SetText()
+    {
+        sensitivityText.text = sensitivitySlider.value.ToString("#.##");
     }
 
     public void SavePrefs()
