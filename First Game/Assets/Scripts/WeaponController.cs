895fb9e540c87a4a225b01a08a53a4701f78fc96
kovada06 <adamkover47@gmail.com>
2021-06-15 17:10:38 -0700

    melee impacts implemented

--- First Game/Assets/Scripts/WeaponController.cs
+++ First Game/Assets/Scripts/WeaponController.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class WeaponController : MonoBehaviour
 {
+    public GameObject swordImpact;
+    private GameObject clone;
+    private Vector3 impactPos;
+
     public Vector3 PrefPosition;
     public Vector3 PrefRotation;
 
@@ -11,12 +16,22 @@ public class WeaponController : MonoBehaviour
 
     public int iWeaponType; // 0: hammer
 
+    private void Awake()
+    {
+        swordImpact = GameObject.Find("SwordImpact");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SendMessage("TakeDamage", weaponDamage);
+            impactPos = other.transform.position;
+            impactPos.y += 1;
+            clone = Instantiate(swordImpact, impactPos, Quaternion.identity);
+            Destroy(clone, 0.5f);
         }
     }
+
 
 }
